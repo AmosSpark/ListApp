a0c06be8e61c58deea67ce67074cd87c244f2678
Ayobami Oguntolu <Ayobamiolugboyega@gmail.com>
2020-02-18 18:29:58 -0800

    no change

--- app.js
+++ app.js
@@ -1,63 +1,60 @@
-
-// variables 
+// variables
 
 const taskInput = document.querySelector(".container__taskInput"),
-    addBtn = document.querySelector(".container__btn-add"),
-    filterTask = document.querySelector(".container__filter"),
-    taskCollection = document.querySelector(".container__collection-list");
-    clearTask = document.querySelector(".container__clear-all");
+  addBtn = document.querySelector(".container__btn-add"),
+  filterTask = document.querySelector(".container__filter"),
+  taskCollection = document.querySelector(".container__collection-list");
+clearTask = document.querySelector(".container__clear-all");
 
 // Event Listener fn
 
 const loadEvents = () => {
-    document.addEventListener("DOMContentLoaded", Store.displayTask);
-    addBtn.addEventListener("click", addTask);
-    filterTask.addEventListener("keyup", filterTasks);
-    clearTask.addEventListener("click", clearAll);
-    taskCollection.addEventListener("click", remTask);
-}
+  document.addEventListener("DOMContentLoaded", Store.displayTask);
+  addBtn.addEventListener("click", addTask);
+  filterTask.addEventListener("keyup", filterTasks);
+  clearTask.addEventListener("click", clearAll);
+  taskCollection.addEventListener("click", remTask);
+};
 
 // Event fn
 
+const addTask = e => {
+  let taskValue = taskInput.value;
+  const task = new Task(taskValue); // Instantiate new task
+  const ui = new UI(); // Instantiate new UI
 
-const addTask = (e) => {
-    let taskValue = taskInput.value;
-    const task = new Task(taskValue); // Instantiate new task
-    const ui = new UI; // Instantiate new UI
-        
-    if (taskValue === "") {
-        ui.showAlert("Add a task","error");
-    } else {
-        ui.showTask(task); // Add task to list
-        Store.addTaskToLoc(task); // Add task to LS
-        ui.showAlert("Task Added","success");
-        ui.clearInput(); // Clear input
-    }
-    e.preventDefault();
-}
-
-const filterTasks = (e) => {
-
-    const ui = new UI() // Instantiate new UI
-    // Filer Task
-    ui.filter(e.target);
+  if (taskValue === "") {
+    ui.showAlert("Add a task", "error");
+  } else {
+    ui.showTask(task); // Add task to list
+    Store.addTaskToLoc(task); // Add task to LS
+    ui.showAlert("Task Added", "success");
+    ui.clearInput(); // Clear input
   }
+  e.preventDefault();
+};
 
-const clearAll = (e) => {
-    const ui = new UI; // Instantiate new UI
-    ui.clearAllTask(); // Clear all task from UI
-    Store.clearTaskFromLoc(); // Clear all task from LS
-    ui.showAlert("All Cleared","success");
-    e.preventDefault();
-}
+const filterTasks = e => {
+  const ui = new UI(); // Instantiate new UI
+  // Filer Task
+  ui.filter(e.target);
+};
 
-const remTask = (e) => {
-    const ui = new UI; // Instantiate new UI
-    ui.delTask(e.target); // Remove task from UI
-    Store.remTaskFromLoc(e.target.parentElement);
-    ui.showAlert("Task Removed","success");
-    e.preventDefault();
-}
+const clearAll = e => {
+  const ui = new UI(); // Instantiate new UI
+  ui.clearAllTask(); // Clear all task from UI
+  Store.clearTaskFromLoc(); // Clear all task from LS
+  ui.showAlert("All Cleared", "success");
+  e.preventDefault();
+};
+
+const remTask = e => {
+  const ui = new UI(); // Instantiate new UI
+  ui.delTask(e.target); // Remove task from UI
+  Store.remTaskFromLoc(e.target.parentElement);
+  ui.showAlert("Task Removed", "success");
+  e.preventDefault();
+};
 
 // Invoke Event Listener fn
 
--- storage.js
+++ storage.js
@@ -1,41 +1,45 @@
 class Store {
-    
-    static getTasks() { // Get task from LS
-        let tasks;
-        if(localStorage.getItem("tasks") === null) {
-            tasks = [];
-        } else {
-            tasks = JSON.parse(localStorage.getItem("tasks"));
-        }
-        return tasks;
+  static getTasks() {
+    // Get task from LS
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"));
     }
+    return tasks;
+  }
 
-    static addTaskToLoc(task) { // Add task to LS
-        const tasks = Store.getTasks();
-              tasks.push(task);
-        localStorage.setItem("tasks",JSON.stringify(tasks));
-    }
+  static addTaskToLoc(task) {
+    // Add task to LS
+    const tasks = Store.getTasks();
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 
-    static displayTask() { // Display task on load
-        const tasks = Store.getTasks();
-        tasks.forEach((task) => {
-            const ui = new UI;
-            ui.showTask(task);
-        });
-    }
+  static displayTask() {
+    // Display task on load
+    const tasks = Store.getTasks();
+    tasks.forEach(task => {
+      const ui = new UI();
+      ui.showTask(task);
+    });
+    ui.showTask(task);
+  }
 
-    static remTaskFromLoc(taskItem) {  // Remove task from LS
-        const tasks = Store.getTasks();
-        tasks.forEach((task,index) => {
-            if(taskItem.firstElementChild.textContent === task) {
-                tasks.splice(index, 1);
-            }
-        });
-        localStorage.setItem("tasks",JSON.stringify(tasks));
-    }
+  static remTaskFromLoc(taskItem) {
+    // Remove task from LS
+    const tasks = Store.getTasks();
+    tasks.forEach((task, index) => {
+      if (taskItem.firstElementChild.textContent === task) {
+        tasks.splice(index, 1);
+      }
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 
-    static clearTaskFromLoc() { // Clear task from LS
-        localStorage.clear();
-    }
+  static clearTaskFromLoc() {
+    // Clear task from LS
+    localStorage.clear();
+  }
 }
-
